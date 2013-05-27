029a67b8b5cfb31b6dd3469efa30a103e29f3906
fandub <pau.lokilla@gmail.com>
2013-05-26 22:07:49 -0300

    Archivo PHP completo

--- prueba.php
+++ prueba.php
@@ -1,21 +1,25 @@
 <?php
 include('lib/nusoap.php');
 
+    //Capturas de datos desde archivo html
     $user1 = $_POST['rut'];
     $user2 = $_POST['digito'];
     $rut = "$user1-$user2";
     
+    // Creacion de arregloc on parametros del webservice
     $parametros=array();
     $parametros['rut']= $rut;
     $parametros['password']= $_POST['Pass'];
     
+    //Uppercase de la clave y trnasformacion a SHA256 de la misma
     $parametros['password']=  strtoupper ( $parametros['password'] );
     $parametros['password']= hash( "sha256", $parametros['password'] );
     
+    //Definimos el webservice a tratar y le enviamos los parametros
     $objClienteSOAP = new soapclient("http://informatica.utem.cl:8011/dirdoc-auth/ws/auth?wsdl");
     $objRespuesta = $objClienteSOAP->autenticar($parametros);
 
-    
+    //print_r($objRespuesta);   Nos permite ver los campos y subcampos del objeto respuesta
     if($objRespuesta->return->codigo == 1)
     {
         echo "Acceso Concedido.";
@@ -28,7 +32,7 @@ include('lib/nusoap.php');
         }
         else
         {
-            echo "Contraseña Y/O Usuario Invalidos.....";
+            echo "Contraseï¿½a Y/O Usuario Invalidos.....";
         }
     }
  ?>
